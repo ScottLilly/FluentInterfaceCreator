c60e0a4888c9e3762506cfc2e6b2d7755ee3d61f
Scott Lilly <scottlilly@scottlilly.com>
2016-09-27 17:04:42 -0500

    Added MethodParameter class
    Added MethodParameter class and project datatypes (populated with C#
    primitive datatypes, by default)

---
+++ Engine/Models/MethodParameter.cs
@@ -0,0 +1,14 @@
+﻿namespace Engine.Models
+{
+    public class MethodParameter
+    {
+        public string Datatype { get; private set; }
+        public string Name { get; private set; }
+
+        public MethodParameter(string datatype, string name)
+        {
+            Datatype = datatype;
+            Name = name;
+        }
+    }
+}
