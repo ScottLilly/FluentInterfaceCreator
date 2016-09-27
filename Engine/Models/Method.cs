68af066d137369c46a36d0b1cbb7c7a9ff71827c
Scott Lilly <scottlilly@scottlilly.com>
2016-09-26 20:00:16 -0500

    Adding initial classes
    Project and Method classes

---
+++ Engine/Models/Method.cs
@@ -0,0 +1,21 @@
+﻿namespace Engine.Models
+{
+    public class Method
+    {
+        public enum Action
+        {
+            Instantiate,
+            Continue,
+            Execute
+        }
+
+        public string Name { get; private set; }
+        public Action ActionToPerform { get; private set; }
+
+        public Method(string name, Action actionToPerform)
+        {
+            Name = name;
+            ActionToPerform = actionToPerform;
+        }
+    }
+}
---
+++ Engine/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Engine.Models
+{
+    public class Project
+    {
+        public string Name { get; private set; }
+        public List<Method> Methods { get; private set; }
+
+        public Project(string name)
+        {
+            Name = name;
+            Methods = new List<Method>();
+        }
+    }
+}
