24d1a88a49a3c9ca2ed0c754d5f891f796e3cd53
Scott Lilly <scottlilly@scottlilly.com>
2016-09-27 20:44:08 -0500

    Adding UI

---
+++ Engine/ViewModels/ProjectEditorViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class ProjectEditorViewModel
+    {
+        public Project CurrentProject { get; private set; }
+    }
+}
---
+++ TestEngine/Models/TestProject.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Engine.Models;
+
+namespace TestEngine.Models
+{
+    [TestClass]
+    public class TestProject
+    {
+        [TestMethod]
+        public void Test_CreateProject()
+        {
+            Project project = new Project("SQLHydra");
+        }
+    }
+}
